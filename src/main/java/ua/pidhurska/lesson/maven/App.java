5617ad33316f56a1d676f67e53992e1788360fc0
Tetiana_Pidhurska <tetiana_pidhurska@epam.com>
2016-10-07 14:41:48 +0300

    Initial commit

--- src/main/java/ua/pidhurska/lesson/maven/App.java
+++ src/main/java/ua/pidhurska/lesson/maven/App.java
@@ -1,13 +1,16 @@
 package ua.pidhurska.lesson.maven;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        if (args == null) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("With arguments"+Arrays.toString(args));
+        }
     }
 }
